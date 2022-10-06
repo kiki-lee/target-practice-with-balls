4c7fe91f9fd5e188466c8e60f225d1157bba4e15
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2022-10-05 17:01:33 -0700

    Cleaned up Assets

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     game.customGameOverExpanded("Great Job!", effects.confetti, scoreTypes.None)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    throwBall = ball.createProjectileBallFromSprite(assets.image`ball0`, myBall)
+    throwBall = ball.createProjectileBallFromSprite(assets.image`ball-blue`, myBall)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Booth, function (sprite, otherSprite) {
     info.changeScoreBy(-1)
@@ -18,16 +18,16 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
 let theTarget: Sprite = null
 let throwBall: Ball = null
 let myBall: Ball = null
-scene.setBackgroundImage(assets.image`wildWest1`)
-myBall = ball.create(assets.image`ball`, SpriteKind.Player)
+scene.setBackgroundImage(assets.image`wildWest`)
+myBall = ball.create(assets.image`ball-yellow`, SpriteKind.Player)
 myBall.setPosition(80, 90)
 myBall.controlWithArrowKeys(true)
-let myBooth = sprites.create(assets.image`boundaries`, SpriteKind.Booth)
+let myBooth = sprites.create(assets.image`booth`, SpriteKind.Booth)
 myBooth.z = 100
 myBall.setTrace(true)
 info.startCountdownGame(20, winTypes.Score)
 forever(function () {
-    theTarget = sprites.createProjectileFromSide(assets.image`target2`, 50, 0)
+    theTarget = sprites.createProjectileFromSide(assets.image`target`, 50, 0)
     theTarget.bottom = 56
     theTarget.setKind(SpriteKind.Enemy)
     pause(randint(500, 2000))
